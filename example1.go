f0e440ac24d351248ddd45cb3d7b92bd41644e48
Borderlord <102431262+Borderlord@users.noreply.github.com>
2020-04-08 17:42:03 +0000

    Update szkt

--- example1.go
+++ example1.go
@@ -1,2 +1,106 @@
 
 package main
+
+import (
+	"fmt"
+	"github.com/adrian3ka/go-learn-ai/grammar_parser"
+	"github.com/adrian3ka/go-learn-ai/helper"
+	"github.com/adrian3ka/go-learn-ai/naive_bayes"
+	nfa2 "github.com/adrian3ka/go-learn-ai/nfa"
+	"github.com/adrian3ka/go-learn-ai/tagger"
+	"github.com/adrian3ka/go-learn-ai/term_frequency"
+	"github.com/adrian3ka/go-learn-ai/tf_idf"
+	"github.com/adrian3ka/go-learn-ai/word_vectorizer"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("============================= Classifier =====================================")
+	wordVectorizer := word_vectorizer.New(word_vectorizer.WordVectorizerConfig{
+		Lower: true,
+	})
+
+	var corpuses map[string][]string
+
+	corpuses = make(map[string][]string)
+
+	corpuses["pulsa"] = []string{
+		"Saya mau beli pulsa dong. Jual voucher gak bang?. Mau isi pulsa dong.",
+		"jual pulsa gak ya?",
+		"kamu jual voucher ga?",
+		"mau isi paket data bisa?",
+		"mau isi pulsa bisa ga ya?",
+	}
+
+	corpuses["tiket"] = []string{
+		"kamu jual tiket pesawat ga?",
+		"disini jual tiket ga ya?",
+		"bisa beli tiket    kereta?",
+		"jual tiket apa  ya?",
+	}
+
+	corpuses["saldo"] = []string{
+		"halo aku mau isi saldo dong",
+		"eh mau topup dong bisa ga?",
+		"mau nambah saldo dong bisa gak",
+		"tolong bantu isi saldo dong 50 ribu",
+	}
+
+	corpuses["hotel"] = []string{
+		"ada kamar kosong ga ya di tempat kamu?",
+		"eh mau sewa 1 kamar dong",
+		"eh  mau sewa ruang meeting dong",
+		"eh fasilitas di hotel kamu apa aja ya?",
+	}
+	err := wordVectorizer.Learn(corpuses)
+
+	if err != nil {
+		panic(err)
+	}
+
+	termFrequency := term_frequency.New(term_frequency.TermFrequencyConfig{
+		Binary:         false,
+		WordVectorizer: wordVectorizer,
+	})
+
+	err = termFrequency.Learn(wordVectorizer.GetCleanedCorpus())
+
+	if err != nil {
+		panic(err)
+	}
+
+	tfIdf, err := tf_idf.New(tf_idf.TermFrequencyInverseDocumentFrequencyConfig{
+		Smooth:          true,
+		NormalizerType:  tf_idf.EuclideanSumSquare,
+		CountVectorizer: termFrequency,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = tfIdf.Fit()
+
+	if err != nil {
+		panic(err)
+	}
+
+	multinomialNB := naive_bayes.NewMultinomialNaiveBayes(naive_bayes.MultinomialNaiveBayesConfig{
+		Evaluator: tfIdf,
+	})
+
+	dataTest := []string{
+		"mAu belI tiket kEreta doNg",
+		"jual pulsa ga ya?",
+		"mau beli tiket kereta pake pulsa bisa ga ya?",
+		"mau topup isi wallet dong",
+		"saya mau beli pulsa dong",
+		"Ada ruang meeting ga di hotel ini ?",
+	}
+
+	predicted, err := multinomialNB.Predict(dataTest)
+
+	if err != nil {
+		panic(err)
