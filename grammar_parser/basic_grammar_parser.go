3feac27282e117a59a3c9d570aa4ac5affc9dfed
Borderlord <102431262+Borderlord@users.noreply.github.com>
2020-09-14 16:09:04 +0000

    Update AGENDA_DIARIA

--- grammar_parser/basic_grammar_parser.go
+++ grammar_parser/basic_grammar_parser.go
@@ -176,3 +176,100 @@ func handleOptional(input *HandleSymbolInput) (*nfa.NFA, []*nfa.State, error) {
 	for idx, _ := range input.PreviousState {
 		newStates = append(newStates, input.PreviousState[idx])
 		err = input.NfaData.AddTransition(input.PreviousState[idx].Index, input.Tag, *state1)
+
+		if err != nil {
+			return nil, nil, err
+		}
+
+	}
+
+	return input.NfaData, newStates, nil
+}
+
+func handleOneOrMore(input *HandleSymbolInput) (*nfa.NFA, []*nfa.State, error) {
+	var newStates []*nfa.State
+	var state1 *nfa.State
+	var err error
+
+	if input.NfaData == nil {
+		input.NfaData, state1, err = nfa.NewNFA(input.Tag, false)
+
+		if err != nil {
+			return nil, nil, err
+		}
+
+	} else {
+
+		state1, err = input.NfaData.AddState(&nfa.State{
+			Name: input.Tag,
+		}, false)
+
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	state2, err := input.NfaData.AddState(&nfa.State{
+		Name: input.Tag,
+	}, input.IsFinal)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if state1 == nil || state2 == nil {
+		return nil, nil, errors.New(NilState)
+	}
+
+	for idx, _ := range input.PreviousState {
+		err = input.NfaData.AddTransition(input.PreviousState[idx].Index, input.Tag, *state1)
+
+		if err != nil {
+			return nil, nil, err
+		}
+	}
+
+	newStates = append(newStates, state2)
+
+	err = input.NfaData.AddTransition(state1.Index, input.Tag, *state2)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = input.NfaData.AddTransition(state2.Index, input.Tag, *state2)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return input.NfaData, newStates, nil
+}
+
+func handleInitialChunking(input *HandleSymbolInput) (*nfa.NFA, []*nfa.State, error) {
+	var newStates []*nfa.State
+	var state1 *nfa.State
+	var err error
+
+	if input.NfaData == nil {
+		input.NfaData, state1, err = nfa.NewNFA(nfa.Negate+input.Tag, false)
+
+		if err != nil {
+			return nil, nil, err
+		}
+
+	} else {
+		return nil, nil, errors.New(ChinkingInitialStateMustBeNil)
+	}
+
+	var tags []string
+	tags = strings.Split(input.Tag, "|")
+
+	for _, tag := range tags {
+		err = input.NfaData.AddTransition(state1.Index, nfa.Negate+tag, *state1)
+	}
+
+	newStates = append(newStates, state1)
+
+	return input.NfaData, newStates, nil
+}
