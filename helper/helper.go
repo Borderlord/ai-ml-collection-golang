4b726aba5f3828d4c816466d09b463b68171b605
Borderlord <102431262+Borderlord@users.noreply.github.com>
2020-10-28 06:18:54 +0000

    add include

--- helper/helper.go
+++ helper/helper.go
@@ -9,4 +9,10 @@ import (
 	"unicode"
 )
 
-func GetStringInBetween(str string, start string, end string) (re
+func GetStringInBetween(str string, start string, end string) (result *string) {
+	s := strings.Index(str, start)
+	if s == -1 {
+		return
+	}
+	s += len(start)
+	e := strings.Index(str, en
