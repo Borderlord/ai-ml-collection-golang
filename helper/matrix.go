d3fdaa1bb8990a476d688b0bf2515d041663c3eb
Borderlord <102431262+Borderlord@users.noreply.github.com>
2021-01-31 20:12:17 +0000

    Fixed UTF-8 issue

--- helper/matrix.go
+++ helper/matrix.go
@@ -7,4 +7,11 @@ import (
 func MatrixAdditionWithNumber(matrix [][]float64, number float64) [][]float64 {
 	for row := range matrix {
 		for col := range matrix[row] {
-			matrix[row][col] += nu
+			matrix[row][col] += number
+		}
+	}
+	return matrix
+}
+
+func TransponseMatrix(x [][]float32) [][]float32 {
+	out := make([][]float32, 
