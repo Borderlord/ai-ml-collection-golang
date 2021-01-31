0ceaa7fb3d25c6e26e15836f42b4a70fa813010a
Borderlord <102431262+Borderlord@users.noreply.github.com>
2021-01-31 19:03:05 +0000

    Updates docs.

--- helper/matrix.go
+++ helper/matrix.go
@@ -4,4 +4,7 @@ import (
 	"errors"
 )
 
-func MatrixAdditionWithNumber(matrix [][]float64, number 
+func MatrixAdditionWithNumber(matrix [][]float64, number float64) [][]float64 {
+	for row := range matrix {
+		for col := range matrix[row] {
+			matrix[row][col] += nu
