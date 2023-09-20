ca36ee8e37a9862482fd1b9ea8cf5d0053d27fcd
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-09-20 21:53:39 +0000

    update coding style

--- tagger/ngram_tagger.go
+++ tagger/ngram_tagger.go
@@ -55,3 +55,89 @@ func (u *UnigramTagger) Predict(text string) ([][2]string, error) {
 		tuple = append(tuple, [2]string{
 			splitedString,
 			*selectedTag,
+		})
+	}
+	return tuple, nil
+}
+
+func (u *UnigramTagger) Learn(tuple [][][2]string) error {
+	var tupleMap = make(map[string]map[string]float64)
+
+	for _, sentence := range tuple {
+		for _, word := range sentence {
+			if _, exists := tupleMap[word[0]]; !exists {
+				var temp = make(map[string]float64)
+				temp[word[1]] = 1
+				tupleMap[word[0]] = temp
+			} else {
+				if _, exists := tupleMap[word[0]][word[1]]; !exists {
+					tupleMap[word[0]][word[1]] = 1
+				} else {
+					tupleMap[word[0]][word[1]] += 1
+				}
+			}
+		}
+	}
+
+	for word, tm := range tupleMap {
+		selectedTag := ""
+		maxCount := float64(0)
+		for tag, count := range tm {
+			if maxCount < count {
+				selectedTag = tag
+				maxCount = count
+			}
+		}
+		u.mapTag[word] = selectedTag
+	}
+
+	return nil
+}
+
+type NGramTagger struct {
+	mapTag        map[string]string
+	n             uint64
+	backoffTagger Tagger
+}
+
+type NGramTaggerConfig struct {
+	BackoffTagger Tagger
+	N             uint64
+}
+
+func NewNGramTagger(cfg NGramTaggerConfig) *NGramTagger {
+	if cfg.N < 2 {
+		cfg.N = 2
+	}
+	n := NGramTagger{
+		backoffTagger: cfg.BackoffTagger,
+		n:             cfg.N,
+	}
+	n.mapTag = make(map[string]string)
+	return &n
+}
+
+func (n *NGramTagger) Predict(text string) ([][2]string, error) {
+	splitedStrings := strings.Split(text, " ")
+	var tuple [][2]string
+	minimumWord := n.n - 1
+
+	for idx, splitedString := range splitedStrings {
+		var selectedTag *string
+
+		if uint64(idx) >= minimumWord && helper.IsLetter(splitedString) {
+
+			generatedTag := ""
+
+			for i := 1; i <= int(minimumWord); i++ {
+				generatedTag += tuple[len(tuple)-i][1] + "_"
+			}
+
+			generatedTag += splitedString
+
+			if helper.IsAlphaUnderscore(generatedTag) {
+				if val, exists := n.mapTag[generatedTag]; exists {
+					if helper.IsLetter(val) {
+						selectedTag = &val
+					}
+				}
