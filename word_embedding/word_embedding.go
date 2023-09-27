7345b439e2d51f60ae7b30b93544d91f1ebb32a7
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-09-27 10:05:08 +0000

    No log message.

--- word_embedding/word_embedding.go
+++ word_embedding/word_embedding.go
@@ -44,3 +44,94 @@ type OneHotEncoderInterface interface {
 }
 
 type WordEmbeddingConfig struct {
+	Dimension          uint64
+	OneHotEncodedData  OneHotEncoderInterface
+	LossFunction       LossFunctionType
+	ActivationFunction ActivationFunctionType
+	Optimizer          OptimizerType
+	LearningRate       float64
+}
+
+func (we *WordEmbedding) Learn(source, target [][]float64) error {
+	if len(source) != len(target) {
+		return errors.New(InvalidDataLearn)
+	}
+
+	for idx := range source {
+		var tempSource, tempTarget [][]float64
+		tempSource = append(tempSource, source[idx])
+		tempTarget = append(tempTarget, target[idx])
+		tempMatrix1, err := helper.MatrixMultiplication(tempSource, we.hiddenLayer.Weight)
+
+		if err != nil {
+			return err
+		}
+
+		we.hiddenLayer.Value = helper.MatrixAdditionWithNumber(tempMatrix1, we.hiddenLayer.Bias)
+
+		tempMatrix2, err := helper.MatrixMultiplication(we.hiddenLayer.Value, we.outputLayer.Weight)
+
+		if err != nil {
+			return err
+		}
+
+		tempMatrix2 = helper.MatrixAdditionWithNumber(tempMatrix2, we.outputLayer.Bias)
+
+		if we.activationFunction == Softmax {
+			tempMatrix2[0] = helper.Softmax(tempMatrix2[0])
+		}
+		fmt.Println(tempMatrix2)
+	}
+
+	return nil
+}
+
+func NewWordEmbedding(config WordEmbeddingConfig) (*WordEmbedding, error) {
+
+	if config.LearningRate == 0 {
+		config.LearningRate = 1
+	}
+	rand.Seed(time.Now().UTC().UnixNano())
+	if config.OneHotEncodedData == nil {
+		return nil, errors.New(OneHotEncodedNil)
+	}
+
+	if len(config.OneHotEncodedData.GetEncodedData()) == 0 {
+		return nil, errors.New(OneHotEncodedNil)
+	}
+
+	hiddenLayer := Layer{
+		Bias: helper.RandFloat(0, 1),
+	}
+
+	for i := 0; i < len(config.OneHotEncodedData.GetEncodedData()[0][0]); i++ {
+		tempRow := helper.RandFloats(0, 1, int(config.Dimension))
+		hiddenLayer.Weight = append(hiddenLayer.Weight, tempRow)
+	}
+
+	outputLayer := Layer{
+		Bias: helper.RandFloat(0, 1),
+	}
+
+	for i := 0; i < int(config.Dimension); i++ {
+		tempRow := helper.RandFloats(0, 1, len(config.OneHotEncodedData.GetEncodedData()[0][0]))
+		outputLayer.Weight = append(outputLayer.Weight, tempRow)
+	}
+
+	we := WordEmbedding{
+		learningRate:       config.LearningRate,
+		outputLayer:        outputLayer,
+		hiddenLayer:        hiddenLayer,
+		oneHotEncodedData:  config.OneHotEncodedData,
+		dimension:          config.Dimension,
+		optimizer:          config.Optimizer,
+		lossFunction:       config.LossFunction,
+		activationFunction: config.ActivationFunction,
+	}
+
+	var source, target [][]float64
+	for _, learnData := range we.oneHotEncodedData.GetEncodedData() {
+		if len(learnData[0]) != len(learnData[1]) {
+			return nil, errors.New(InvalidDataLearn)
+		}
+		var tempSource, tempTarget []float64
