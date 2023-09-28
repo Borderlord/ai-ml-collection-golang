f92eb67b4c9df7acabe628cf93d501979ef614b0
Borderlord <102431262+Borderlord@users.noreply.github.com>
2023-09-28 17:20:52 +0000

    Create run.sh

--- word_vectorizer/array_word_vectorizer.go
+++ word_vectorizer/array_word_vectorizer.go
@@ -6,4 +6,10 @@ import (
 )
 
 const (
-	InvalidType = "Invalid Ty
+	InvalidType = "Invalid Type"
+)
+
+type ArrayWordVectorizer struct {
+	lower          bool
+	data           map[string]uint64
+	labelEncoded  
